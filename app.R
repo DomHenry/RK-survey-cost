1785e28c46fbef448c9f3411b4302ffd45df9d0b
Dominic Henry <dominichenry@gmail.com>
2020-05-24 21:19:18 +0200

    Replaced inputs with sliders, added box numbering and new "download" menuitem

--- app.R
+++ app.R
@@ -10,8 +10,9 @@ ui <- dashboardPage(
 
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Comparison", tabName = "complot", icon = icon("dashboard")),
-      menuItem("User data", tabName = "userdata", icon = icon("th"))
+      menuItem("Comparison", tabName = "complot", icon = icon("chart-area")),
+      menuItem("User data", tabName = "userdata", icon = icon("th")),
+      menuItem("Download", tabName = "download", icon = icon("download"))
     )
   ),
 
@@ -27,52 +28,70 @@ ui <- dashboardPage(
 
         fluidRow(
           box(
-            title = "Speed and rates input", width = 4,
+            title = "1. Speed and rates input", width = 3,
             numericInput("survey_speed_input", "Survey speed (km/h)", value = 35, min = 10, max = 80, step = 5),
             numericInput("labour_rate", "Input the labour rate ($/hour):", value = 25, step = 5),
-            numericInput("mileage_rate", "Input the mileage rate ($/km):", value = 0.89, step = 0.1),
+            numericInput("mileage_rate", "Input the mileage rate ($/km):", value = 0.89, step = 0.1)
+          ),
+          box(
+            title = "2. Survey distances", width = 3,
+            # numericInput("trans_dist_1", "Transect distance 1 (km):", value = 50, step = 10),
+            sliderInput("trans_dist_1", "Transect distance 1 (km):", min= 0, max = 350, value = 30, step = 5),
+            sliderInput("trans_dist_2", "Transect distance 2 (km):", min= 0, max = 350, value = 60, step = 5),
+            sliderInput("trans_dist_3", "Transect distance 3 (km):", min= 0, max = 350, value = 90, step = 5),
+            sliderInput("trans_dist_4", "Transect distance 4 (km):", min= 0, max = 350, value = 150, step = 5)
+          ),
+          box(
+            title = "3. Study durations", width = 3,
+            # numericInput("study_day1", "Study duration 1 (days):", value = 10, step = 5),
+            sliderInput("study_day1", "Study duration 1 (days):", min= 10, max = 90, value = 10, step = 1),
+            sliderInput("study_day2", "Study duration 2 (days):", min= 10, max = 90, value = 30, step = 1),
+            sliderInput("study_day3", "Study duration 3 (days):", min= 10, max = 90, value = 50, step = 1)
+          ),
+          box(
+            title = "4. Budget", width = 3,
             radioButtons("maxcost", "Display maximum budget limit?", c("Yes" = "yes", "No" = "no")),
             numericInput("maxcost_amount", "Enter your maximum budget ($)", value = 0, step = 500)
-          ),
-          box(
-            title = "Survey distances", width = 4,
-            numericInput("trans_dist_1", "Transect distance 1 (km):", value = 50, step = 10),
-            numericInput("trans_dist_2", "Transect distance 2 (km):", value = 60, step = 10),
-            numericInput("trans_dist_3", "Transect distance 3 (km):", value = 70, step = 10),
-            numericInput("trans_dist_4", "Transect distance 4 (km):", value = 80, step = 10)
-          ),
-          box(
-            title = "Study durations", width = 4,
-            numericInput("study_day1", "Study duration 1 (days):", value = 10, step = 5),
-            numericInput("study_day2", "Study duration 2 (days):", value = 30, step = 5),
-            numericInput("study_day3", "Study duration 3 (days):", value = 60, step = 5)
           )
         ),
+        
         fluidRow(
           box(
-            title = "Download data", width = 2,
-            downloadButton("downloadData", "Download cost analysis data")
-          )
-        ),
-
-        fluidRow(
-          box(
-            title = "Comparitve plot", width = 4,
+            title = "Comparitve plot", width = 8,
             plotOutput("panel_plot")
           ),
           box(
             title = "Study cost for daily surveys", width = 4,
-            tableOutput("cost_data")
+            tableOutput("cost_data") #TODO sort this table by distance
           )
         )
       ),
+      
       tabItem(
         tabName = "userdata",
-
+       
         fluidRow(
           box(
             title = "Data frame", width = 8,
             dataTableOutput("cost_data_dynamic")
+          )
+        )
+      ),
+      
+      tabItem(
+        tabName = "download",
+        
+        fluidRow(
+          box(
+            title = "Download data", width = 4,
+            downloadButton("downloadData", "Download cost analysis data")
+          )
+        ),
+        
+        fluidRow(
+          box(
+            title = "Download plot", width = 4,
+            downloadButton("downloadPlot", "Download cost analysis plot")
           )
         )
       )
@@ -84,6 +103,7 @@ source("functions/generate_cost_data.R")
 source("functions/cost_plot.R")
 
 server <- function(input, output, session) {
+  
   cost_table <- reactive({
     generate_cost_data(
       input$mileage_rate, input$labour_rate,
@@ -94,6 +114,16 @@ server <- function(input, output, session) {
     )
   })
 
+  cost_panel_plot <- reactive({
+    
+    cost_plot(
+      cost_table(), input$survey_speed_input,
+      input$study_day1,
+      input$study_day2, input$study_day3, input$maxcost, input$maxcost_amount
+    )
+    
+  })
+  
   output$cost_data <- renderTable(
     {
       cost_table() %>%
@@ -113,17 +143,12 @@ server <- function(input, output, session) {
     options = list(pagelength = 5)
   )
 
-
   output$panel_plot <- renderPlot(
     {
-      cost_plot(
-        cost_table(), input$survey_speed_input,
-        input$study_day1,
-        input$study_day2, input$study_day3, input$maxcost, input$maxcost_amount
-      )
+      cost_panel_plot()
     },
     res = 96,
-    # width = 800, height = 800
+    width = 800, height = 500
   )
 
   output$downloadData <- downloadHandler(
@@ -133,6 +158,15 @@ server <- function(input, output, session) {
     content = function(file) {
       write.csv(cost_table(), file, row.names = FALSE)
     }
+  )
+    output$downloadPlot <- downloadHandler(
+      filename = function(){
+        paste0(Sys.Date(), " cost_analysis_data.png")
+        },
+      content = function(file){
+        ggsave(file,
+               plot = cost_panel_plot())
+      }
   )
 }
 
