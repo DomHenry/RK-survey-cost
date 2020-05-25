b6485b96444e849d65e9ff3b5b3b4389d06d1fd4
Dominic Henry <dominichenry@gmail.com>
2020-05-25 15:59:35 +0200

    New function for maxcost plot

---
+++ functions/cost_max_plot.R
@@ -0,0 +1,29 @@
+cost_max_plot <- function(x,
+                          study_day1, study_day2, study_day3,
+                          trans_dist_1,trans_dist_2,trans_dist_3,trans_dist_4){
+  
+  three_study_duration <- as.character(sort(c(study_day1, study_day2, study_day3)))
+  four_transect_distance <- sort(c(trans_dist_1,trans_dist_2,trans_dist_3,trans_dist_4))
+  
+  cbp2 <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
+            "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  x %>% 
+    filter(carcass_detection == 100) %>% 
+    distinct(transect_distance,study_duration_days,total_cost) %>% 
+    mutate(transect_distance = as.character(transect_distance)) %>% 
+    ggplot(aes(x = study_duration_days, y = total_cost,fill = transect_distance))+
+    geom_bar(stat = "identity", width = 0.7, position = position_dodge(width = 0.8))+
+    scale_x_discrete(limits = three_study_duration)+
+    xlab("Study duration (days)") +
+    ylab("Total study cost")+
+    scale_fill_manual(values = cbp2[c(3,4,5,7)],limits = four_transect_distance)+
+    labs(fill = str_wrap("Transect distance (km)", width = 15))+
+    theme_bw()+
+    theme(axis.text = element_text(size = 10),
+          axis.title = element_text(size = 11),
+          strip.text = element_text(size = 10),
+          legend.title = element_text(size = 10),
+          legend.text = element_text(size = 10))+
+    scale_y_continuous(labels = scales::dollar_format())
+  
+}
