fcf5b6266799372274a148b8fada9fe03ef3f8f5
Dominic Henry <dominichenry@gmail.com>
2020-05-25 21:44:02 +0200

    Removed uneccessary arrange()

--- functions/generate_cost_data.R
+++ functions/generate_cost_data.R
@@ -43,8 +43,7 @@ generate_cost_data <- function(mileage_rate, labour_rate,
            total_cost = labour_cost + mileage_cost) %>% 
     mutate_at(vars(persistence, carcass_detection,
                    transect_distance, study_duration_days,
-                   survey_speed), as_factor) %>% 
-    arrange(transect_distance,study_duration_days)
+                   survey_speed), as_factor)
   
   return(cost)
 }
