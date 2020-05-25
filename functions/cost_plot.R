c6d885c48357fd8832057ef55f53b3bdec501cb5
Dominic Henry <dominichenry@gmail.com>
2020-05-25 15:58:30 +0200

    Reduced plot text size

--- functions/cost_plot.R
+++ functions/cost_plot.R
@@ -15,17 +15,18 @@ cost_plot <- function(x,survey_speed_input,
     ggplot(aes(x = persistence, y = total_cost,color = transect_distance, group = carcass_detection))+
     geom_point(size = 2)+
     geom_line(aes(group = transect_distance), size = 1)+
-    labs(title = glue("Speed: {survey_speed_input}km/h"))+
+    labs(title = glue("Survey speed: {survey_speed_input}km/h"))+
     scale_colour_manual(values=cbp2[c(3,4,5,7)])+
     labs(color = str_wrap("Transect distance (km)", width = 15))+
     xlab("Mean carcass persistence (days)") +
     ylab("Total study cost")+
     theme_bw()+
-    theme(axis.text = element_text(size = 12),
-          axis.title = element_text(size = 14),
-          strip.text = element_text(size = 12),
-          legend.title = element_text(size = 14),
-          legend.text = element_text(size = 12))+
+    theme(title = element_text(size = 10),
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 11),
+          strip.text = element_text(size = 10),
+          legend.title = element_text(size = 10),
+          legend.text = element_text(size = 10))+
     scale_y_continuous(labels = scales::dollar_format()) + 
     facet_grid(carcass_detection ~ study_duration_days,
                labeller = labeller(carcass_detection = detect_labs))
