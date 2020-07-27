1a97d39181c95d07e8524704a57b637a318c0201
Dominic Henry <dominichenry@gmail.com>
2020-07-27 16:51:24 +0200

    Added bold text to variable names

--- functions/instruction_text.R
+++ functions/instruction_text.R
@@ -12,22 +12,21 @@ in_text <- tags$div(
               of the app and associated paper published in XXX.")
     )
   ),
-  tags$b(tags$p("Survey Design")),
+  tags$b(tags$p("Survey Design")), 
   tags$p("The Survey Design analysis takes a number of user defined inputs in the blue boxes 
          (via numeric inputs and sliders) across four main input categories (speed and rates, 
          survey distance, study duration, and budget). Each category is numbered and will 
          allow you to sequentially select values that are relevant to the type of survey designs you 
          wish to compare."),
-  tags$p("Survey speed is the average speed of the vehicle driven along the transects, labour rate is 
-         the cost per hour of employing staff who conduct the surveys, and the mileage rate is the 
-         cost per km of the vehicle used for the transects (note that mileage should include the 
-         combined cost of fuel and maintenance)."),
-  tags$p("You then have a choice to select four different survey distances which represent the one-way 
-         distance of a roadkill survey transect."),
-  tags$p("There are three options for the study duration which indicate the total time available to 
+  tags$p(tags$em(tags$b("Survey speed")), "is the average speed of the vehicle driven along the transects,",
+         tags$em(tags$b("labour rate")),"is the cost per hour of employing staff who conduct the surveys, and the 
+         ", tags$em(tags$b("mileage rate")), "is the cost per km of the vehicle used for the transects (note that mileage should include the combined cost of fuel and maintenance)."),
+  tags$p("You then have a choice to select four different", tags$em(tags$b("survey distances")) ,
+  "which represent the one-way distance of a roadkill survey transect."),
+  tags$p("There are three options for the ", tags$em(tags$b("study durations"))," which indicate the total time available to 
          complete the roadkill surveys."),
   tags$p("The budget box gives an option to plot a red, dotted horizontal line which represents the 
-         maximum funds available for the surveys based on the sum of mileage and labour costs."),
+         ", tags$em(tags$b("maximum funds"))," available for the surveys based on the sum of mileage and labour costs."),
   tags$p("The primary outputs are two figures (in green boxes) which compare how costs change in 
          relation to the mean carcass persistence across the various survey scenarios at either 75% or 50% 
          detection rates (which arise from conducting an alternative to daily surveys). The costs of doing 
@@ -42,6 +41,6 @@ in_text <- tags$div(
          hours worked during surveys (both daily and total), and a breakdown of mileage and labour costs."),
   tags$b(tags$p("Citations")),
   tags$p("Here you'll find links to the peer-reviewed publication, the 
-         GitHub repo for the R Shiny app and the recommended citation for both."),
-  tags$p("If you experience any problems, feel free to contact us at dominch@ewt.org.za")
+         GitHub repo for the R Shiny app, and the recommended citation for both."),
+  tags$p("If you experience any problems, feel free to contact us at *csu@ewt.org.za*")
 )
